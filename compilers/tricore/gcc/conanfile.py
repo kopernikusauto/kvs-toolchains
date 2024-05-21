783ca7ef0354db7b49fd86ae409ae6aaf42e7a87
Chip Collier <chip@kopernikusauto.com>
2024-05-21 14:43:26 +0200

    Tricore GCC recipe

--- compilers/tricore/gcc/conanfile.py
+++ compilers/tricore/gcc/conanfile.py
@@ -3,44 +3,48 @@ from conan import ConanFile
 from conan.tools.files import get, copy
 from conan.errors import ConanInvalidConfiguration
 
-PREFIX = "x86_64-centos7-linux-gnu"
+PREFIX = "tricore-elf"
 
-class Gcc13Toolchain(ConanFile):
-    name = "x86_64-centos7-linux-gnu-gcc"
-    version = "13.2.0"
-    description = "Conan package for GCC 13.2.0 built with glibc-2.17 using crosstool-ng"
+ARCHIVES = {
+    "Linux": ("https://github.com/kopernikusauto/kvs-toolchains/releases/download/2024.01.25/tricore-elf-gcc-11.3.1.tar.xz",
+              "de02d8befcbac36f3bc9f1b06ec0e2059b3b2d6411b2259152cac05eb5d4267c"),
+    "Windows": ("https://github.com/kopernikusauto/kvs-toolchains/releases/download/2024.01.25/llvm-mingw-20240417-ucrt-x86_64.zip",
+                "afa69ac40f08721658bbd6826b633f3b54579d7ae4cab1f624cc6e2efd05bf0e"),
+}
+
+class TricoreGccToolchain(ConanFile):
+    name = "tricore-gcc"
+    version = "11.3.1"
+    description = "Conan package for GCC 11.3.1"
     license = "GPL-3.0-only"
     package_type="application"
     settings = "os", "arch"
 
     def validate(self):
-        if self.settings.arch != "x86_64" or self.settings.os != "Linux":
-            raise ConanInvalidConfiguration(f"This toolchain is not compatible with {self.settings.os}-{self.settings.arch}. "
-                                            "It can only run on Linux-x86_64.")
+        if self.settings.arch != "x86_64":
+            raise ConanInvalidConfiguration(f"This toolchain is not compatible with arch: {self.settings.arch}.")
+        if self.settings.os != "Linux" and self.settings.os != "Windows":
+            raise ConanInvalidConfiguration(f"This toolchain is only available on Windows and Linux")
 
-        if self.settings_target.os != "Linux" or self.settings_target.arch != "x86_64":
-            raise ConanInvalidConfiguration(f"This toolchain only supports building for Linux-x86_64. "
-                                        f"{self.settings_target.os}-{self.settings_target.arch} is not supported.")
+    def _sha(self):
+        return ARCHIVES[str(self.settings.os)][1]
 
-        if self.settings_target.compiler != "gcc":
-            raise ConanInvalidConfiguration(f"The compiler is set to '{self.settings_target.compiler}', but this "
-                                            "toolchain only supports building with gcc.")
+    def _url(self):
+        return ARCHIVES[str(self.settings.os)][0]
 
     def build(self):
-        get(self,
-            "https://github.com/kopernikusauto/kvs-toolchains/releases/download/2024.01.25/gcc_13.2.0_x86_64-centos7-linux-gnu.tar.xz",
-            sha256="8c456531d4c24d35e20b491fe3b29c22ac61ff6b38e437e282f55ad9c18f1b6b")
+        url, sha = ARCHIVES[str(self.settings.os)]
+        get(self, url, sha256=sha)
 
     def package_id(self):
         self.info.settings_target = self.settings_target
         # We only want the ``arch`` setting
-        self.info.settings_target.rm_safe("os")
         self.info.settings_target.rm_safe("compiler")
         self.info.settings_target.rm_safe("build_type")
 
     def package(self):
         toolchain = PREFIX
-        dirs_to_copy = [toolchain, "bin", "include", "lib", "libexec", "etc", "share"]
+        dirs_to_copy = ["bin", "include", "lib", "libexec", "mcs-elf", "share", PREFIX]
         for dir_name in dirs_to_copy:
             copy(self, pattern=f"{toolchain}/{dir_name}/*", src=self.build_folder, dst=self.package_folder, keep_path=True)
         copy(self, "LICENSE", src=self.build_folder, dst=os.path.join(self.package_folder, "licenses"), keep_path=False)
---
+++ compilers/tricore/gcc/test_package/conanfile.py
@@ -0,0 +1,18 @@
+
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "CMakeToolchain", "VirtualBuildEnv"
+
+    def build_requirements(self):
+        self.tool_requires(self.tested_reference_str)
+
+    def layout(self):
+        cmake_layout(self)
+
+    def test(self):
+        toolchain = "tricore-elf"
+        self.run(f"{toolchain}-gcc --version")
